dd7b9d14871e13ace323a7cbe06cc147ffebfed2
S-human-S <powergodofthe666@gmail.com>
2025-02-15 11:42:02 +0900

    Create script.js

---
+++ script.js
@@ -0,0 +1,9 @@
+function unV(){
+  let info = document.getElementById('college');
+  info.style.display = "none";
+document.getElementById('college2');
+  info.style.display = "block";
+let random = Math.floor( Math.random() * 101 );
+let percent = document.getElementById('P');
+P.textcontent = random + "%";
+}
